f6fab542cf527eb606a14ad8a85c5779e6e5a9f4
bizhermit <rsenda@bizhermit.com>
2022-01-11 19:50:16 +0900

    add react-sdk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppStyle } from "@biz-hermit/react-sdk/dist/styles/style";
 import type { AppProps } from "next/app";
 import "../styles/base.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return <AppStyle design="neumorphism" color="dark"><Component {...pageProps} /></AppStyle>;
 };
 export default App;
--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -1,6 +1,12 @@
+import MessageBox from "@biz-hermit/react-sdk/dist/components/message-box";
+import GroupBox from "@biz-hermit/react-sdk/dist/containers/group-box";
+import Row from "@biz-hermit/react-sdk/dist/containers/row";
+import Button from "@biz-hermit/react-sdk/dist/controls/button";
+import useMessageBox from "@biz-hermit/react-sdk/dist/hooks/message-box";
+import { StyleContext } from "@biz-hermit/react-sdk/dist/styles/style";
 import { NextPage } from "next";
 import Link from "next/link";
-import { VFC } from "react";
+import { useContext, VFC } from "react";
 
 const SandboxPage: NextPage = () => {
     return <SandboxComponent />;
@@ -9,10 +15,41 @@ const SandboxPage: NextPage = () => {
 export default SandboxPage;
 
 const SandboxComponent: VFC = () => {
+    const style = useContext(StyleContext);
+    const msgbox = useMessageBox();
     return (
         <>
         <h1>Sandbox</h1>
         <Link href="/">index</Link>
+        <Row>
+            <GroupBox caption="color">
+                <Button click={() => {
+                    style.setColor(null);
+                }}>unset</Button>
+                <Button click={() => {
+                    style.setColor("light");
+                }}>Light</Button>
+                <Button click={() => {
+                    style.setColor("dark");
+                }}>Dark</Button>
+            </GroupBox>
+            <GroupBox caption="design">
+                <Button click={() => {
+                    style.setDesign(null);
+                }}>unset</Button>
+                <Button click={async (unlock) => {
+                    // style.setDesign("flat");
+                    await msgbox.alert("Unimplemented...");
+                    unlock();
+                }}>Flat</Button>
+                <Button click={() => {
+                    style.setDesign("material");
+                }}>Material</Button>
+                <Button click={() => {
+                    style.setDesign("neumorphism");
+                }}>Neumorphism</Button>
+            </GroupBox>
+        </Row>
         </>
     );
 };
